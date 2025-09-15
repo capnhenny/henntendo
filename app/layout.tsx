5597b2cd168e1bcc512252a457ecac841e7ee0bc
capnhenny <capnhenny@gmail.com>
2025-09-14 23:26:59 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Henntendo",
+  description: "My very own game-focused easter eggs",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
