693142ab31ce981753ce2de410a7bd0349493ddf
capnhenny <capnhenny@gmail.com>
2025-09-15 22:50:13 -0400

    Create SurfBG.tsx

---
+++ app/components/SurfBG.tsx
@@ -0,0 +1,5 @@
+// app/components/SurfBG.tsx
+export default function SurfBG() {
+  // Minimal background container; CSS handles visuals.
+  return <div className="surf-bg" aria-hidden="true" />;
+}
