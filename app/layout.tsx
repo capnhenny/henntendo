e20703026912940e59b26e8d5fa538d42db141b2
capnhenny <capnhenny@gmail.com>
2025-09-19 17:07:11 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,7 @@ import type { Metadata } from "next";
 import "./styles.css";
 
 import { VT323, Oxanium, Bebas_Neue } from "next/font/google";
-
 import SurfBG from "./components/SurfBG";
-// ...
-<body className={`theme-arcade ${bodyFont.className} ${displayFont.variable} ${bebasFont.variable}`}>
-  <SurfBG />
-  {children}
-</body>
 
 const bodyFont    = VT323({ subsets: ["latin"], weight: "400" });
 const displayFont = Oxanium({ subsets: ["latin"], weight: ["400","700"], variable: "--font-display" });
@@ -18,21 +12,14 @@ const bebasFont   = Bebas_Neue({ subsets: ["latin"], weight: "400", variable: "-
 export const metadata: Metadata = {
   title: "Henntendo",
   description: "My very own game-focused easter eggs",
-  icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png",
-  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/* expose next/font vars on <body> so CSS can use --font-display and --font-bebas */}
+      {/* Body gets the font classes/variables */}
       <body className={`theme-arcade ${bodyFont.className} ${displayFont.variable} ${bebasFont.variable}`}>
+        <SurfBG />
         {children}
 
         <footer className="site-footer">
